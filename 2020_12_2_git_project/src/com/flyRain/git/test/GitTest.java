4b142ab2f7c835bd323b87ae4445c3e561c59661
flyRain <wang10013512@163.com>
2020-12-02 10:30:16 +0800

    提交git-project项目到本地仓库

---
+++ 2020_12_2_git_project/src/com/flyRain/git/test/GitTest.java
@@ -0,0 +1,15 @@
+package com.flyRain.git.test;
+
+/**
+ * Copyright (C),2020-2020,武汉厚溥教育科技有限公司
+ * FileName：2020_12_2_git_project
+ * Author：  联想
+ * Date：    2020/12/2  10:19
+ * Description：测试git
+ * Version： 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test to commit");
+    }
+}
